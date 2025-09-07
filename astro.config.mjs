315f92c5a45644c9b45c0d6868a0b070bb1d8467
wjlucc <wjlucc@gmail.com>
2025-09-08 01:58:14 +0800

    增加Giscus

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
-// TODO: 将 site 设置为你的站点地址（用于 RSS/Sitemap 等功能）。示例：
-// site: 'https://<your-username>.github.io/linespark'
 export default defineConfig({
+  site: 'https://wjlucc.github.io/linespark',
+  base: '/linespark',
   integrations: [mdx()],
 });
